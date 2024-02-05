4dbb79fc3562bd40c637fc9257278d555ee987d5
Varad Deshpande <111638914+varaddeshpande15@users.noreply.github.com>
2024-02-05 08:52:12 +0530

    Update buzzer.ino

--- basic_codes/buzzer/buzzer.ino
+++ basic_codes/buzzer/buzzer.ino
@@ -1,1 +1,11 @@
+int buzzerPin = 8;
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+void loop() {
+  digitalWrite(buzzerPin, HIGH);
+  delay(1000); // 1 sec
+  digitalWrite(buzzerPin, LOW);
+  delay(500); // 0.5 sec 
+}
 
