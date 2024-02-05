422db987154b54dee70f54e7028851c8eacdaec3
Varad Deshpande <111638914+varaddeshpande15@users.noreply.github.com>
2024-02-05 08:54:35 +0530

    Update rotating_servo_motor_at_different_angles.ino

--- basic_codes/servo_motor/rotating_servo_motor_at_different_angles.ino
+++ basic_codes/servo_motor/rotating_servo_motor_at_different_angles.ino
@@ -1,1 +1,18 @@
+#include<Servo.h>
+Servo s1;
+void setup() {
+  s1.attach(9);
+}
 
+void loop() {
+  s1.write(0);
+  delay(1000);
+  s1.write(45);
+  delay(1000);
+  s1.write(90);
+  delay(1000);
+  s1.write(135);
+  delay(1000);
+  s1.write(180);
+  delay(1000);
+}
