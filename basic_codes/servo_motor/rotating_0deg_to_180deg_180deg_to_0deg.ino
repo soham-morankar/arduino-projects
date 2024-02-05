8ca1758684e1fdb9f3736bfda2d5e6bd1d3abf8d
Varad Deshpande <111638914+varaddeshpande15@users.noreply.github.com>
2024-02-05 08:57:09 +0530

    Update rotating_0deg_to_180deg_180deg_to_0deg.ino

--- basic_codes/servo_motor/rotating_0deg_to_180deg_180deg_to_0deg.ino
+++ basic_codes/servo_motor/rotating_0deg_to_180deg_180deg_to_0deg.ino
@@ -1,1 +1,18 @@
+#include<Servo.h>
+Servo s1;
+int angle;
+void setup() {
+  s1.attach(9);
 
+}
+
+void loop() {
+  for(angle = 0;angle<180;angle++){
+    s1.write(angle);
+    delay(15);
+  }
+  for(angle=180;angle>0;angle--){
+    s1.write(angle);
+    delay(15);
+  }
+}
