bed434e95e2c1436cd12734b1ceab76cf52adba2
Varad Deshpande <111638914+varaddeshpande15@users.noreply.github.com>
2024-02-05 08:44:08 +0530

    Update blink.ino

--- basic_codes/blink_a_led/blink.ino
+++ basic_codes/blink_a_led/blink.ino
@@ -1,1 +1,11 @@
-
+void setup() {
+// initialize digital pin 11 as an output.
+     pinMode(11, OUTPUT);
+}
+// the loop function runs over and over again forever
+void loop() {
+     digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);              // wait for a second
+    digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
+    delay(100);              // wait for a second
+}
